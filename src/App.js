c087b58c8f11338b8318b099953c4add86ae6c45
Arshad Ali <arshuarshad8019@gmail.com>
2023-12-13 18:28:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddTodoAction } from './Actions/TodoActions';
+import { RemoveTodoAction } from './Actions/TodoActions';
 
 function App() {
+
+  const [todo, setTodo]=useState();
+
+  const dispatch =useDispatch();
+  const Todo =useSelector((state) => state.Todo);
+  const {todos} =Todo;
+
+  const handleSubmit =(e) =>{
+    e.preventDefault();
+    dispatch(AddTodoAction(todo));
+  }
+  const removeHandler=(t)=>{
+    dispatch(RemoveTodoAction(t));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header p-5 ">
+        <h2>Todo List App in Redux</h2>
+        <form className='flex pt-6' onSubmit={handleSubmit}>
+          <input className=' w-4/5 px-10 rounded-xl border-none text-black text-xl' placeholder='Enter a Todo' onChange={(e)=>setTodo(e.target.value)}/>
+          <button className='p-4 bg-white rounded-2xl text-black text-sm ml-5' type='submit'>Go</button>
+        </form>
+        <ul className='list-none w-4/5 m-0 p-0'>
+          {
+            todos && todos.map((t)=>(
+              <li key={t.id} className='rounded my-2 mx-0 bg-blue-400 py-2 px-5 flex items-center'>
+            <span className='flex-1 text-start'>{t.todo}</span>
+            <button onClick={()=>removeHandler(t)} className='rounded p-3 border border-solid border-white text-white bg-orange-600'>Delete</button>
+          </li>
+            ))
+          }
+          
+        </ul>
       </header>
     </div>
   );
